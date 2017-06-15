98b59bc2c626d2dac02df8d47a7961f190be36a6
Verma, Prashant <prashantverma@qainfotech.com>
2017-06-15 11:41:21 +0530

    added caliper-event-model.js

---
+++ caliper-event-model.js
@@ -0,0 +1,12 @@
+window.caliper.event=(function(){
+   var event = {
+  	@context : null,
+  	@type : null,
+  	actor : {},
+  	action : null,
+  	object : {},
+  	eventTIme = null
+ }
+ return event;
+})();
+
